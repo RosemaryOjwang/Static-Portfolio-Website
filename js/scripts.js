20c09dde077c860f1a718ad7d4b4f9bc39be180c
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-11-18 22:44:36 +0300

    add setTimeout() for #myJob1, #myJob2 and #myNameDelay

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
-const message1 = document.getElementById("message1");
-message1.setAttribute("class", "hidden");
-const message = document.getElementById("message");
-message.setAttribute("class", "hidden");
+function getAndSetAttributes() {
+    const message1 = document.getElementById("message1");
+    message1.setAttribute("class", "hidden");
+    const message = document.getElementById("message");
+    message.setAttribute("class", "hidden");
+}
 
-window.onload = function()  {
-    let button = document.querySelecto("button");
-    button.onclick = function(event)    {
-        event.delayDefault();
-       let message1D= document.querySelector("#message1");
-       let messageD = document.querySelector("#message");
-       message.removeAttribute("class");
-       message1.removeAttribute("class");
+window.onload = function () {
+    let button = document.querySelector("button");
+    button.onclick = function (event) {
+        event.preventDefault();
+        getAndSetAttributes();
+        message.removeAttribute("class");
+        message1.removeAttribute("class");
     };
+    let myNameDelayed = document.getElementById("myNameDelay");
+    function myNameDelay() {
+        myNameDelayed.innerText = "Hi, I'm Rosemary Ojwang";
+    }
+    setTimeout(myNameDelay, 500);
+    let myJob1 = document.getElementById("myJob1");
+    function myJob1Delay() {
+        myJob1.innerText = "Front-end Web Developer";
+    }
+    setTimeout(myJob1Delay, 2000);
+    let myJob2 = document.getElementById("myJob2");
+    function myJob2Delay() {
+        myJob2.innerText = "UX/UI Developer";
+    }
+    setTimeout(myJob2Delay, 3500);
 }
